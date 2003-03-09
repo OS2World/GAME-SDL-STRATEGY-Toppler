ae5b65f3f9f7b76e1cda6eda8c9aee065ecf9f43
roever <unknown>
2003-03-09 10:13:18 +0000

    makefile cleanup

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-for i in config.sub config.guess; do
-	if test ! -e "$i"; then ln -s "/usr/share/automake-1.6/$i" .; fi
-done
-
+libtoolize --automake
 aclocal
 autoheader
 automake --gnu --add-missing
 autoconf
-./configure
+./configure $@
