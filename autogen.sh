8c8b20d8f357b2973f280209f7270cd9e7e8fc0b
Andreas R?ver <roever@users.sourceforge.net>
2005-01-03 17:42:54 +0000

    update to build-system (VOLKER)

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
+echo "$0: running autopoint ..."
+autopoint
+
+echo "$0: running post-treatments for autopoint ..."
+(
+	extra_dist="`cd m4/ && ls -1 *.m4 | tr '\n' ' '`"
+	linguas="`cd po/ && ls -1 *.po | cut -d. -f1`"
+	all_linguas="`echo "$linguas" | tr '\n' ' '`"
+	tempfile="`mktemp -p.`"
+
+	echo "Creating m4/Makefile.am"
+	echo "EXTRA_DIST = $extra_dist" >m4/Makefile.am
+
+	echo "Creating po/ChangeLog"
+	>po/ChangeLog
+
+	echo "Changing configure.ac"
+	sed 's/\(ALL_LINGUAS="\)[^"]*/\1'"$all_linguas"'/' <configure.ac >"$tempfile" \
+	&& cat <"$tempfile" >configure.ac
+
+	rm -f "$tempfile"
+)
+
+echo "$0: running aclocal ..."
 aclocal
-autoheader
-automake --gnu --add-missing
+
+echo "$0: running autoconf ..."
 autoconf
+
+echo "$0: running autoheader ..."
+autoheader
+
+echo "$0: running automake ..."
+automake --gnu --add-missing
+
+echo "$0: running configure ..."
 ./configure $@
