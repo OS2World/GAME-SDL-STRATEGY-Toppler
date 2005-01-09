3ae913964cb084622148a455a22b65a7cfe8d28b
Andreas R?ver <roever@users.sourceforge.net>
2005-01-09 09:46:58 +0000

    bild cleanup part II (Volker)

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo "$0: running autopoint ..."
-autopoint
+autopoint || exit 1
 
 echo "$0: running post-treatments for autopoint ..."
 (
 	extra_dist="`cd m4/ && ls -1 *.m4 | tr '\n' ' '`"
 	linguas="`cd po/ && ls -1 *.po | cut -d. -f1`"
-	all_linguas="`echo "$linguas" | tr '\n' ' '`"
-	tempfile="`mktemp -p.`"
+	all_linguas="`echo -n "$linguas" | tr '\n' ' '`"
 
 	echo "Creating m4/Makefile.am"
 	echo "EXTRA_DIST = $extra_dist" >m4/Makefile.am
@@ -16,24 +15,25 @@ echo "$0: running post-treatments for autopoint ..."
 	echo "Creating po/ChangeLog"
 	>po/ChangeLog
 
-	echo "Changing configure.ac"
-	sed 's/\(ALL_LINGUAS="\)[^"]*/\1'"$all_linguas"'/' <configure.ac >"$tempfile" \
-	&& cat <"$tempfile" >configure.ac
+	echo "Creating po/LINGUAS"
+	echo -n "$linguas" >po/LINGUAS
 
-	rm -f "$tempfile"
+	echo "Updating configure.ac (backup is in configure.ac~)"
+	cp configure.ac configure.ac~
+	sed 's/\(ALL_LINGUAS="\)[^"]*/\1'"$all_linguas"'/' <configure.ac~ >configure.ac
 )
 
 echo "$0: running aclocal ..."
-aclocal
+aclocal || exit 1
 
 echo "$0: running autoconf ..."
-autoconf
+autoconf || exit 1
 
 echo "$0: running autoheader ..."
-autoheader
+autoheader || exit 1
 
 echo "$0: running automake ..."
-automake --gnu --add-missing
+automake --gnu --add-missing || exit 1
 
 echo "$0: running configure ..."
-./configure $@
+./configure $@ || exit 1
