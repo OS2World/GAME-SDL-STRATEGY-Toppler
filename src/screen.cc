b771f725b8b0ebb35cd7391c473a4c7e4a4c9e92
Andreas Röver <roever@users.sf.net>
2022-01-23 16:02:11 +0100

    this define is needed for windows

--- src/screen.cc
+++ src/screen.cc
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
+#define _USE_MATH_DEFINES
 #include "screen.h"
 
 #include "archi.h"
